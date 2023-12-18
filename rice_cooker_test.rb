e260514777e52f3073c68e7482ebecc9d1b73ac1
Aina RASOANAIVO <103510206+AinaR07@users.noreply.github.com>
2023-12-19 00:03:25 +0100

    Update rice_cooker_test.rb

--- rice_cooker_test.rb
+++ rice_cooker_test.rb
@@ -1,41 +1,45 @@
-require 'minitest/autorun'
+
 require_relative 'RiceCooker'
+require 'rspec'
+require 'tty-prompt'
 
-class RiceCookerTest < Minitest::Test
-  def setup
-    @RiceCooker = riceCooker.new
+RSpec.describe 'VotreApplication' do
+    describe '#choisir_mode' do
+      it 'affiche un message de choix non valide' do
+        allow_any_instance_of(TTY::Prompt).to receive(:select).and_return('Autre aliment')
+        allow_any_instance_of(TTY::Prompt).to receive(:ask).and_return('po')
+        allow_any_instance_of(TTY::Prompt).to receive(:select).and_return('Son')
+        allow_any_instance_of(TTY::Prompt).to receive(:select).and_return('Éteindre')
+        expect { choisir_mode }.to output(/Choix non valide/).to_stdout
+      end
+    end
+
+  describe '#determiner_temps' do
+    it 'retourne le temps de cuisson pour les modes prédéfinis' do
+      prompt = TTY::Prompt.new
+      expect(determiner_temps('Riz Blanc', prompt)).to eq(2)
+      expect(determiner_temps('Riz Complet', prompt)).to eq(2)
+      expect(determiner_temps('Cuisson Vapeur', prompt)).to eq(2)
+    end
+
+    it 'retourne le temps de cuisson pour "Autre aliment"' do
+      allow_any_instance_of(TTY::Prompt).to receive(:ask).and_return('10')
+      prompt = TTY::Prompt.new
+      expect(determiner_temps('Autre aliment', prompt)).to eq(10)
+    end
+
+    it 'retourne 0 pour un temps de cuisson invalide pour "Autre aliment"' do
+      allow_any_instance_of(TTY::Prompt).to receive(:ask).and_return('-5')
+      prompt = TTY::Prompt.new
+      expect(determiner_temps('Autre aliment', prompt)).to eq(0)
+    end
   end
 
-  def test_cuire_riz
-    assert_output(/Le riz est cuit.*Cuisson terminée/) { cuire_riz }
-  end
-
-  def test_cuisson_vapeur
-    assert_output(/Veuillez Patientez SVP.*Cuisson terminée/) { cuisson_vapeur }
-  end
-
-  def test_cuisson_cereales
-    assert_output(/Cuisson des céréales terminée.*Ca y est c'est prêt/) { cuisson_cereales }
-  end
-
-  def test_cuisson_soupe
-    assert_output(/Cuisson terminée.*La soupe est prête/) { cuisson_soupe }
-  end
-
-  def test_bouillir_eau
-    assert_output(/tic tic tic tic.*L'eau est chaude/) { bouillir_eau }
-  end
-
-  def test_cuisson_dessert
-    assert_output(/Cuisson terminée.*C'est prêt/) { cuisson_dessert }
-  end
-
-  def test_maintenir_chaud
-    assert_output(/Maintenir au chaud.../) { maintenir_chaud }
-  end
-
-  def test_choisir_option
-    input = "3\n"
-    assert_output(/Choisissez une option : /) { $stdin = StringIO.new(input); choisir_option }
+  describe '#afficher_alertes' do
+    it 'affiche les alertes disponibles' do
+      allow_any_instance_of(TTY::Prompt).to receive(:select).and_return('Son')
+      prompt = TTY::Prompt.new
+      expect { afficher_alertes(prompt, 10) }.to output(/Types d'alertes disponibles/).to_stdout
+    end
   end
 end
